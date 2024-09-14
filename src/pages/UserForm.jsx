a89fcb2daf7c7469bbd8646f683303dbae18aaff
Holewalker <dani75esp@gmail.com>
2024-09-14 21:34:57 +0200

    Added "not_set" option to user company

--- src/pages/UserForm.jsx
+++ src/pages/UserForm.jsx
@@ -373,7 +373,7 @@ function UserForm() {
                                         required
                                         variant="outlined"
                                     >
-                                        <MenuItem value="" disabled>Select a company</MenuItem>
+                                        <MenuItem value="not_set">All companies</MenuItem> {/* Opción "not_set" */}
                                         {companies.map((company) => (
                                             <MenuItem key={company.id} value={company.id}>
                                                 {company.name}
