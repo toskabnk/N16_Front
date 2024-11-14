fa9f3af5be2deabf7ed6c9c7eda253a521594c3c
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2024-11-14 23:46:53 +0100

    Added version in sidebar

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { version } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +12,7 @@ export default defineConfig({
       ],
     }),
   ],
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
 })
